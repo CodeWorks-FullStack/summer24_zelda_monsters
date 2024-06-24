830c3dd0760b1d889234b1b8799af6989647ef6b
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-24 11:40:36 -0600

    using fetch to request data

--- app/controllers/MonstersController.js
+++ app/controllers/MonstersController.js
@@ -4,6 +4,19 @@ export class MonstersController {
   constructor() {
     console.log('Loaded Monsters Controller');
     this.getMonsters()
+    // NOTE asynchronous code allows the code to pause in the method below, but continue running other actions outside of that
+    // this.testPromise()
+  }
+  async testPromise() {
+    // When we await this promise, the code in this method stops running, and will only continue after the promise has been resolved
+    const myPromise = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // NOTE this promise resolves in 3 seconds
+        resolve("Promise has been resolved");
+      }, 3000);
+    });
+    // NOTE this line starts running after Promise is resolved
+    console.log(myPromise);
   }
 
   getMonsters() {
--- app/services/MonstersService.js
+++ app/services/MonstersService.js
@@ -1,6 +1,11 @@
 class MonstersService {
-  getMonsters() {
-    console.log('getting the monsters!');
+  async getMonsters() {
+    const response = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters')
+    console.log('FETCH BOY 🐕🗞️', response);// promise not resolved
+    const data = await response.json()
+    console.log('parsed data to json', data);
+
+    data.data.forEach(mon => console.log(mon.name))
   }
 }
 
