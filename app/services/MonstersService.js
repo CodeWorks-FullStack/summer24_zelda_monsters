95bc5e721ed7db1d523bbe89f13a352d89935ead
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-24 11:44:47 -0600

    added notes on fetch

--- app/services/MonstersService.js
+++ app/services/MonstersService.js
@@ -1,10 +1,15 @@
 class MonstersService {
+  // NOTE every method that requests data from an API should be async
   async getMonsters() {
+    // NOTE we await the Promise to be resolved from fetch so that we can access the data
+    // REVIEW we wait for the dog to return with the ball
     const response = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters')
-    console.log('FETCH BOY 🐕🗞️', response);// promise not resolved
+    console.log('FETCH BOY 🐕🗞️', response)
+    // NOTE convert the response to JSON so we can work with the data
     const data = await response.json()
-    console.log('parsed data to json', data);
+    console.log('parsed data to json', data)
 
+    // response body looked like this: {data: [{name: 'jeremy', ...}, {name: 'mick', ...}]}
     data.data.forEach(mon => console.log(mon.name))
   }
 }
